277c0b71bf6e16583fe9646b57c8e61c6c3eaf47
TenZ <sparshosarker0077@gmail.com>
2026-03-21 16:51:04 +0600

    chore: remove accidental gods_EYE dev artifacts and harden ignores

--- gods_EYE/gods_EYE.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-setlocal
-cd /d "E:\gods_EYE"
-.\target\release\gods_EYE.exe %*
-endlocal
--- gods_EYE/gods_EYE.ps1
+++
@@ -1,4 +0,0 @@
-$originalLocation = Get-Location
-Set-Location "E:\gods_EYE"
-.\target\release\gods_EYE.exe $args
-Set-Location $originalLocation
